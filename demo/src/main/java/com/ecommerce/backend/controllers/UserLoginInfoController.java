21daa12e387c7fd2e438128e0ebaa27a013768d4
criticalH1T <82686036+criticalH1T@users.noreply.github.com>
2023-11-29 21:19:06 +0100

    UserLoginInfoRepo changes

--- demo/src/main/java/com/ecommerce/backend/controllers/UserLoginInfoController.java
+++ demo/src/main/java/com/ecommerce/backend/controllers/UserLoginInfoController.java
@@ -4,13 +4,15 @@ import com.ecommerce.backend.dtos.UserLoginInfoDto;
 import com.ecommerce.backend.entities.UserLoginInfo;
 import com.ecommerce.backend.mappers.UserLoginInfoMapper;
 import com.ecommerce.backend.repositories.UserLoginInfoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/user-login-info")
 public class UserLoginInfoController {
 
     private final UserLoginInfoRepository userLoginInfoRepository;
@@ -22,11 +24,22 @@ public class UserLoginInfoController {
         this.userLoginInfoMapper = userLoginInfoMapper;
     }
 
-    @GetMapping("/user_login_info")
+    @GetMapping
     public List<UserLoginInfoDto> getAllLoginInfo() {
         List<UserLoginInfo> loginInfoList = userLoginInfoRepository.findAll();
         return loginInfoList.stream()
                 .map(userLoginInfoMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping("/save-user")
+    public ResponseEntity<String> saveUserLoginInfo(@RequestBody UserLoginInfo userLoginInfo) {
+        try {
+            userLoginInfoRepository.save(userLoginInfo);
+            return new ResponseEntity<>("User login info saved.", HttpStatusCode.valueOf(200));
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(e.getCause().toString(), HttpStatusCode.valueOf(500));
+        }
     }
 }
